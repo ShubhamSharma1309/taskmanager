94bfa18c9f03cbba4890c7048d4bf9842c15b181
vishalmishraa <vishal.mishra9470@gmail.com>
2024-09-19 13:30:30 +0530

    implemented redux , shadcn , theming

---
+++ client/lib/redux/store.ts
@@ -0,0 +1,35 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { TypedUseSelectorHook, useSelector, useDispatch } from 'react-redux';
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const makeStore = () => {
+    return configureStore({
+        reducer: persistedReducer,
+        middleware: (getDefaultMiddleware) =>
+            getDefaultMiddleware({
+                serializableCheck: false,
+            }),
+    });
+};
+
+export type AppStore = ReturnType<typeof makeStore>;
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppDispatch = AppStore['dispatch'];
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const store = makeStore();
+export const persistor = persistStore(store);
