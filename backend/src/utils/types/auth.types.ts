d62eb02f34156db829a699dc52b9258e6a67d20b
vishalmishraa <vishal.mishra9470@gmail.com>
2024-09-19 11:51:11 +0530

    password validation

--- backend/src/utils/types/auth.types.ts
+++ backend/src/utils/types/auth.types.ts
@@ -2,5 +2,5 @@ import { z } from 'zod';
 
 export const LoginSchema = z.object({
     email: z.string().email(),
-    password: z.string(),
+    password: z.string().min(8, { message: "Password should be at least 8 characters" }),
 });
--- backend/src/utils/types/user.types.ts
+++ backend/src/utils/types/user.types.ts
@@ -4,7 +4,7 @@ import { Document } from 'mongoose';
 export const UserSchema = z.object({
     name: z.string(),
     email: z.string().email(),
-    password: z.string(),
+    password: z.string().min(8, { message: "Password should be at least 8 characters" }),
     confirmPassword: z.string(),
 }).refine(data => data.password === data.confirmPassword, {
     message: "Passwords don't match",
@@ -14,4 +14,3 @@ export const UserSchema = z.object({
 export interface IUser extends z.infer<typeof UserSchema>, Document {
     comparePassword: (password: string) => Promise<boolean>;
 }
-
