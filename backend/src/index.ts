107a764caac14f9a04ea0c14ec2c7bde11b17692
vishalmishraa <vishal.mishra9470@gmail.com>
2024-09-22 02:09:16 +0530

    setup build

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,15 @@ import authRouter from './routers/auth.router';
 import taskRouter from './routers/task.router';
 
 const app = express();
-connectDB();
+connectDB().then(() => {
+    if (process.env.NODE_ENV !== 'production') {
+        app.listen(configs.PORT, () => {
+
+            console.log(`Server running on PORT: ${configs.PORT}`);
+            console.log('Running in development mode');
+        });
+    }
+}).catch((err:any)=>console.log(err));
 
 //app use cors
 app.use(cors({
@@ -17,6 +25,8 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
+console.log(configs.CORS_ORIGIN);
+
 
 //app use config
 app.use(express.json());
@@ -34,9 +44,5 @@ app.use((_req, _res, next) => {
     next(new ErrorHandler(404, 'Not found'));
 });
 app.use(errorMiddleware);
-
-app.listen(configs.PORT, () => {
-    console.log(`server running on PORT : ${configs.PORT}`);
-});
 
 export default app;
