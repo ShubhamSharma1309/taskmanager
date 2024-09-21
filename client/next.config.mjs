251c39bb0139e716c75c04c60ffefdeb09978e01
vishalmishraa <vishal.mishra9470@gmail.com>
2024-09-21 16:48:44 +0530

    add env to nextconfig

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,5 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    env: {
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
+    },
     typescript: {
         ignoreBuildErrors: true
     },
