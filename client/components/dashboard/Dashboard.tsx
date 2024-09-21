654194935874ce1d5c4380c04b2e0341e3ae2928
vishalmishraa <vishal.mishra9470@gmail.com>
2024-09-22 00:40:56 +0530

    fix touch dnd

--- client/components/dashboard/Dashboard.tsx
+++ client/components/dashboard/Dashboard.tsx
@@ -1,12 +1,14 @@
 import useDashboard from '@/hooks/use-dashboard'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
+import { TouchBackend } from 'react-dnd-touch-backend'
 import { Column } from './Column'
+import { isMobile } from 'react-device-detect'
 
 const Dashboard = () => {
   const { moveTask, tasks } = useDashboard();
   return (
-    <DndProvider backend={HTML5Backend}>
+    <DndProvider backend={isMobile ? TouchBackend : HTML5Backend}>
       <div className="container mx-auto mt-14 p-4">
         <div className="flex flex-col md:flex-row gap-4">
           <Column status="TODO" tasks={tasks} moveTask={moveTask} />
